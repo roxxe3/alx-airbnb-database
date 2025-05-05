0642ab9409801526b2285576eb1c510327b41fb0
Hamza Farissi <hamzafarissi@gmail.com>
2025-05-05 17:11:27 +0100

    push

---
+++ database-script-0x01/schema.sql
@@ -0,0 +1,74 @@
+create TABLE USER (
+    user_id CHAR(36) PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(15) UNIQUE,
+    role ENUM('guest', 'host', 'admin') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    INDEX(user_id),
+    INDEX(email),
+);
+
+CREATE TABLE PROPERTY (
+    property_id CHAR(36) PRIMARY KEY,
+    host_id CHAR(36)
+    Foreign Key (host_id) REFERENCES User(user_id),
+    name VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    price_per_night DECIMAL NOT NULL,
+    CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    INDEX(property_id),
+    INDEX(booking_id)
+);
+
+CREATE TABLE BOOKING (
+    booking_id CHAR(36) PRIMARY KEY,
+    property_id CHAR(36),
+    user_id CHAR(36),
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    total_price DECIMAL NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (property_id) REFERENCES Property(property_id),
+    status ENUM('pending', 'confirmed', 'cancelled') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    INDEX(booking_id),
+    INDEX(property_id)
+);
+
+CREATE TABLE PAYMENT (
+    payment_id CHAR(36) PRIMARY KEY,
+    booking_id CHAR(36),
+    amount DECIMAL NOT NULL,
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    payment_method ENUM('credit_card', 'paypal', 'stripe') NOT NULL,
+    FOREIGN KEY (booking_id) REFERENCES Booking(booking_id),
+    INDEX(payment_id)
+);
+
+create TABLE REVIEW (
+    review_id CHAR(36) PRIMARY KEY,
+    property_id CHAR(36),
+    user_id CHAR(36),
+    rating INT CHECK (rating >= 1 AND rating <= 5),
+    comment TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (property_id) REFERENCES Property(property_id),
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    INDEX(review_id)
+);
+
+CREATE TABLE MESSAGE (
+    message_id CHAR(36) PRIMARY KEY,
+    sender_id CHAR(36),
+    recipient_id CHAR(36),
+    message_body TEXT NOT NULL,
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (sender_id) REFERENCES User(user_id),
+    FOREIGN KEY (receiver_id) REFERENCES User(user_id),
+    INDEX(message_id)
+);
